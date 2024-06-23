2a8c153bdbe9be9de0d7af6237220f42a4b2b241
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-22 21:03:10 -0500

    pregunta 2 inicio

--- src/app/principal/salario/salario.component.ts
+++ src/app/principal/salario/salario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-salario',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './salario.component.html',
   styleUrl: './salario.component.css'
 })
