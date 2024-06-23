d0dc52f38ad7c0e7d9a359feebf7de350358baec
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-22 21:28:56 -0500

    pregunta 3 terminada

--- src/app/principal/materias/materias.component.ts
+++ src/app/principal/materias/materias.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../angular-material/material/material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-materias',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule,CommonModule],
   templateUrl: './materias.component.html',
   styleUrl: './materias.component.css'
 })
 export class MateriasComponent {
 
+  matricula: number | null = null;
+  costo: number = 520;
+
+  calcular(materias: string): void {
+    const nummat = parseInt(materias, 10);
+
+    if (isNaN(nummat) || nummat < 0) {
+      this.matricula = null;
+      return;
+    }
+
+    let total = nummat * this.costo;
+
+    if (nummat > 5) {
+      total = total * 0.90; // Aplicar 10% de descuento
+    }
+
+    this.matricula = total;
+  }
 }
