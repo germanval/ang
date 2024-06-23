08f71bc1115e06014a0b0a139c8aa35489680eeb
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-22 21:47:32 -0500

    pequeño detalle faltante de la pregunta 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { PrincipalComponent } from './principal/principal.component';
 import { MateriasComponent } from './principal/materias/materias.component';
 import { SalarioComponent } from './principal/salario/salario.component';
 import { TraficoComponent } from './principal/trafico/trafico.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path:"login",component:LoginComponent},
     {path:"principal",component:PrincipalComponent,
+        canActivate: [authGuard],
         children:[
             {path:"materias",component:MateriasComponent},
             {path:"salario",component:SalarioComponent},
