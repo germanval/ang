2dc0def7c9d717a5eb06ed655c9c4d399757020f
germanval <germanvalcarcel_1500@hotmail.com>
2024-06-22 21:18:25 -0500

    pregunta 3 inicio

--- src/app/principal/materias/materias.component.ts
+++ src/app/principal/materias/materias.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-materias',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './materias.component.html',
   styleUrl: './materias.component.css'
 })
